c2348c1db337500b6083a663d6508b026d4fc80e
Shamyralieva Zeinep <zeinepshamyralieva01@gmail.com>
2024-09-15 21:59:42 +0600

    Created the 1 level Card.tsx component

---
+++ src/components/Card.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+interface CardProps {
+    rank: string;
+    suit: string;
+}
+
+const suitSymbols: { [key: string]: string } = {
+    diams: '♦',
+    hearts: '♥',
+    clubs: '♣',
+    spades: '♠',
+};
+
+const Card: React.FC<CardProps> = ({ rank, suit }) => {
+    return (
+        <span className={`card rank-${rank} ${suit}`}>
+      <span className="rank">{rank}</span>
+      <span className="suit">{suitSymbols[suit]}</span>
+    </span>
+    );
+};
+
+export default Card;
